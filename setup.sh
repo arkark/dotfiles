d51d56bf0727d395e32bb3d04894060d2460c3d1
Ark <target.ark@gmail.com>
2018-07-24 17:26:45 +0900

    [fix] Link to .Xmodmap

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if [ ! -d "$HOME/dotfiles" ]; then
     exit 1
 fi
 
-DOT_FILES=( .bash_profile .bashrc .gdbinit .gitconfig .inputrc .powerline-shell.json emmet )
+DOT_FILES=( .Xmodmap .bash_profile .bashrc .gdbinit .gitconfig .inputrc .powerline-shell.json emmet )
 
 for file in ${DOT_FILES[@]}; do
     ln -sni $HOME/dotfiles/$file $HOME/$file
