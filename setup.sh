9eb674361a9041fc67b98146cf045f459b69e79b
Takeshi Kaneko <target.ark@gmail.com>
2019-06-24 03:31:24 +0900

    Remove .uncrustify

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ if [ ! -d "$HOME/dotfiles" ]; then
     exit 1
 fi
 
-DOT_FILES=( .Xmodmap .bash_profile .bashrc .gdbinit .gitconfig .inputrc .powerline-shell.json .prettierignore .uncrustify emmet )
+DOT_FILES=( .Xmodmap .bash_profile .bashrc .gdbinit .gitconfig .inputrc .powerline-shell.json .prettierignore emmet )
 
 for file in ${DOT_FILES[@]}; do
     ln -sni $HOME/dotfiles/$file $HOME/$file
