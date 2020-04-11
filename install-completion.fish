fe3346d8baaf453e9bd48de07cb9cb9d64d73071
Takeshi Kaneko <target.ark@gmail.com>
2020-04-11 18:38:54 +0900

    Add install-completion.fish

---
+++ install-completion.fish
@@ -0,0 +1,35 @@
+#!/usr/bin/env fish
+
+if ! test -d $HOME/dotfiles
+    echo "Clone arkark/dotfiles into $HOME"
+    exit 1
+end
+
+# docker
+# npm
+# yarn
+# rustup
+# cargo
+# aws-cli
+
+# docker-compose
+if type docker-compose >/dev/null 2>&1
+    echo -e "\e[36m ~/.config/fish/completions/docker-compose.fish\e[0m"
+    curl -L https://raw.githubusercontent.com/docker/compose/v1.25.2/contrib/completion/fish/docker-compose.fish -o ~/.config/fish/completions/docker-compose.fish \
+        && echo -e "\e[32m-> succeeded\e[0m"
+end
+
+# dub
+if type dub >/dev/null 2>&1
+    echo -e "\e[36m~/.config/fish/completions/dub.fish\e[0m"
+    curl -L https://raw.githubusercontent.com/dlang/dub/v1.20.0/scripts/fish-completion/dub.fish -o ~/.config/fish/completions/dub.fish \
+        && echo -e "\e[32m-> succeeded\e[0m"
+end
+
+
+# gradle
+if type gradle >/dev/null 2>&1
+    echo -e "\e[36m~/.config/fish/completions/gradle.fish\e[0m"
+    curl -L https://raw.githubusercontent.com/fish-shell/fish-shell/3.1.0/share/completions/gradle.fish -o ~/.config/fish/completions/gradle.fish \
+        && echo -e "\e[32m-> succeeded\e[0m"
+end
