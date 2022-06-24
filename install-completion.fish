aa85967c5535450210daeb3012d3d139e93102d1
Takeshi Kaneko <target.ark@gmail.com>
2022-06-24 19:29:27 +0900

    Update completions

--- install-completion.fish
+++ install-completion.fish
@@ -21,21 +21,20 @@ end
 # docker-compose
 if type docker-compose >/dev/null 2>&1
     echo -e "\e[36m ~/.config/fish/completions/docker-compose.fish\e[0m"
-    curl -L https://raw.githubusercontent.com/docker/compose/v1.25.2/contrib/completion/fish/docker-compose.fish -o ~/.config/fish/completions/docker-compose.fish \
+    curl -L https://raw.githubusercontent.com/docker/compose/1.25.x/contrib/completion/fish/docker-compose.fish -o ~/.config/fish/completions/docker-compose.fish \
         && echo -e "\e[32m-> succeeded\e[0m"
 end
 
 # dub
 if type dub >/dev/null 2>&1
     echo -e "\e[36m~/.config/fish/completions/dub.fish\e[0m"
-    curl -L https://raw.githubusercontent.com/dlang/dub/v1.20.0/scripts/fish-completion/dub.fish -o ~/.config/fish/completions/dub.fish \
+    curl -L https://raw.githubusercontent.com/dlang/dub/v1.29.0/scripts/fish-completion/dub.fish -o ~/.config/fish/completions/dub.fish \
         && echo -e "\e[32m-> succeeded\e[0m"
 end
-
 
 # gradle
 if type gradle >/dev/null 2>&1
     echo -e "\e[36m~/.config/fish/completions/gradle.fish\e[0m"
-    curl -L https://raw.githubusercontent.com/fish-shell/fish-shell/3.1.0/share/completions/gradle.fish -o ~/.config/fish/completions/gradle.fish \
+    curl -L https://raw.githubusercontent.com/fish-shell/fish-shell/3.5.0/share/completions/gradle.fish -o ~/.config/fish/completions/gradle.fish \
         && echo -e "\e[32m-> succeeded\e[0m"
 end
